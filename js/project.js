84f0a10fe31086d70b28aa4cc90aac28f8e2812d
Tom Lelievre <153502878+LelievreTom@users.noreply.github.com>
2024-12-14 11:30:23 +0000

    bug projets

--- js/project.js
+++ js/project.js
@@ -5,22 +5,32 @@ document.addEventListener('DOMContentLoaded', function () {
     const nextBtn = document.getElementById('nextProject');
     let currentProject = 0;
 
+    // S'assurer que les projets sont cachés et qu'un seul projet est visible
     if (projects.length > 0) {
-        projects[currentProject].classList.add('active');
+        projects.forEach((project, index) => {
+            if (index !== currentProject) {
+                project.classList.remove('active'); // Cache les autres projets
+            } else {
+                project.classList.add('active'); // Affiche le premier projet
+            }
+        });
 
         prevBtn.addEventListener('click', function () {
+            // Masque l'actuel projet
             projects[currentProject].classList.remove('active');
+            // Passe au projet précédent
             currentProject = (currentProject - 1 + projects.length) % projects.length;
+            // Affiche le nouveau projet
             projects[currentProject].classList.add('active');
         });
 
         nextBtn.addEventListener('click', function () {
+            // Masque l'actuel projet
             projects[currentProject].classList.remove('active');
+            // Passe au projet suivant
             currentProject = (currentProject + 1) % projects.length;
+            // Affiche le nouveau projet
             projects[currentProject].classList.add('active');
         });
     }
-
-
-
 });
