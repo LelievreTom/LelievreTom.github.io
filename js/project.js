049b98fe2fddb61e00ce14519b0963d0b25d8f93
Tom Lelievre <153502878+LelievreTom@users.noreply.github.com>
2024-12-14 11:44:14 +0000

    projects

--- js/project.js
+++ js/project.js
@@ -5,31 +5,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const nextBtn = document.getElementById('nextProject');
     let currentProject = 0;
 
-    // S'assurer que les projets sont cachés et qu'un seul projet est visible
+    // Cache tous les projets au départ
     if (projects.length > 0) {
-        projects.forEach((project, index) => {
-            if (index !== currentProject) {
-                project.classList.remove('active'); // Cache les autres projets
-            } else {
-                project.classList.add('active'); // Affiche le premier projet
-            }
-        });
+        projects[currentProject].classList.add('active'); // Affiche le premier projet
 
         prevBtn.addEventListener('click', function () {
-            // Masque l'actuel projet
             projects[currentProject].classList.remove('active');
-            // Passe au projet précédent
             currentProject = (currentProject - 1 + projects.length) % projects.length;
-            // Affiche le nouveau projet
             projects[currentProject].classList.add('active');
         });
 
         nextBtn.addEventListener('click', function () {
-            // Masque l'actuel projet
             projects[currentProject].classList.remove('active');
-            // Passe au projet suivant
             currentProject = (currentProject + 1) % projects.length;
-            // Affiche le nouveau projet
             projects[currentProject].classList.add('active');
         });
     }
